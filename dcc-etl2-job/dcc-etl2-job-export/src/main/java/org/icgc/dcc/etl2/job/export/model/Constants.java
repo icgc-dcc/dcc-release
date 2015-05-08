8428e99c21e02112aa85d5bf0b35abd9ac4278b7
Sepehr Mavedati <sepehr.mavedati@oicr.on.ca>
2015-05-08 13:24:31 -0400

    Implemented donor family and donor therapy data type export.

--- dcc-etl2-job/dcc-etl2-job-export/src/main/java/org/icgc/dcc/etl2/job/export/model/Constants.java
+++ dcc-etl2-job/dcc-etl2-job-export/src/main/java/org/icgc/dcc/etl2/job/export/model/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
   public static final String OPEN_FIELD_VALUE = "OPEN";
   public static final String CONTROLLED_FIELD_VALUE = "CONTROLLED";
   public static final String MASKED_FIELD_VALUE = "MASKED";
+
   public static final String CNSM_TYPE_FIELD_VALUE = "cnsm";
   public static final String EXP_ARRAY_TYPE_FIELD_VALUE = "exp_array";
   public static final String EXP_SEQ_TYPE_FIELD_VALUE = "exp_seq";
@@ -19,8 +20,10 @@ public class Constants {
   public static final String PEXP_TYPE_FIELD_VALUE = "pexp";
   public static final String STSM_TYPE_FIELD_VALUE = "stsm";
 
-  public static final String SAMPLE_FIELD_NAME = "sample";
   public static final String EXPOSURE_FIELD_NAME = "exposure";
+  public static final String FAMILY_FIELD_NAME = "family";
+  public static final String THERAPY_FIELD_NAME = "therapy";
+  public static final String SAMPLE_FIELD_NAME = "sample";
   public static final String SPECIMEN_FIELD_NAME = "specimen";
   public static final String OBSERVATION_FIELD_NAME = "observation";
   public static final String CONSEQUENCE_FIELD_NAME = "consequence";
