19d216d3d32747c571d5636ace6497d7677c3008
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2016-11-30 14:56:55 -0500

    Moved biomarker and surgery to donor (#11)

--- dcc-release-job/dcc-release-job-join/src/main/java/org/icgc/dcc/release/job/join/function/CombineSpecimen.java
+++ dcc-release-job/dcc-release-job-join/src/main/java/org/icgc/dcc/release/job/join/function/CombineSpecimen.java
@@ -19,42 +19,27 @@ package org.icgc.dcc.release.job.join.function;
 
 import static org.icgc.dcc.common.core.model.FieldNames.DONOR_SAMPLE;
 import static org.icgc.dcc.common.core.model.FieldNames.SubmissionFieldNames.SUBMISSION_SPECIMEN_ID;
-import static org.icgc.dcc.release.core.util.FieldNames.JoinFieldNames.BIOMARKER;
-import static org.icgc.dcc.release.core.util.FieldNames.JoinFieldNames.SURGERY;
 import static org.icgc.dcc.release.job.join.utils.JsonNodes.populateArrayNode;
-import lombok.val;
 
 import org.apache.spark.api.java.function.Function;
-import org.icgc.dcc.common.core.model.FieldNames;
-
-import scala.Tuple2;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Optional;
 
-public class CombineSpecimen implements Function<Tuple2<String, Tuple2<Tuple2<Tuple2<ObjectNode,
-    Optional<Iterable<ObjectNode>>>, Optional<Iterable<ObjectNode>>>, Optional<Iterable<ObjectNode>>>>, ObjectNode> {
+import lombok.val;
+import scala.Tuple2;
+
+public class CombineSpecimen
+    implements Function<Tuple2<String, Tuple2<ObjectNode, Optional<Iterable<ObjectNode>>>>, ObjectNode> {
 
   @Override
-  public ObjectNode call(Tuple2<String, Tuple2<Tuple2<Tuple2<ObjectNode, Optional<Iterable<ObjectNode>>>,
-      Optional<Iterable<ObjectNode>>>, Optional<Iterable<ObjectNode>>>> tuple) throws Exception {
-    val specimenSampleTuple = tuple._2._1._1;
+  public ObjectNode call(Tuple2<String, Tuple2<ObjectNode, Optional<Iterable<ObjectNode>>>> tuple) throws Exception {
+    val specimenSampleTuple = tuple._2;
     val specimen = specimenSampleTuple._1;
+
     if (specimenSampleTuple._2.isPresent()) {
       val samples = specimen.withArray(DONOR_SAMPLE);
       populateArrayNode(samples, specimenSampleTuple._2.get(), CombineSpecimen::trimSample);
-    }
-
-    val biomarkerTuple = tuple._2._1;
-    if (biomarkerTuple._2.isPresent()) {
-      val biomarker = specimen.withArray(BIOMARKER);
-      populateArrayNode(biomarker, biomarkerTuple._2.get(), CombineSpecimen::trimDonorFields);
-    }
-
-    val surgeryTuple = tuple._2;
-    if (surgeryTuple._2.isPresent()) {
-      val surgery = specimen.withArray(SURGERY);
-      populateArrayNode(surgery, surgeryTuple._2.get(), CombineSpecimen::trimDonorFields);
     }
 
     return specimen;
@@ -62,13 +47,6 @@ public class CombineSpecimen implements Function<Tuple2<String, Tuple2<Tuple2<Tu
 
   private static ObjectNode trimSample(ObjectNode node) {
     node.remove(SUBMISSION_SPECIMEN_ID);
-
-    return node;
-  }
-
-  private static ObjectNode trimDonorFields(ObjectNode node) {
-    node.remove(FieldNames.SubmissionFieldNames.SUBMISSION_DONOR_ID);
-    node.remove(FieldNames.PROJECT_ID);
 
     return node;
   }
