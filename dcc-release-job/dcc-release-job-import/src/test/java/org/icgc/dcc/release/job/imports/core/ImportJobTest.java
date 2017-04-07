48fc196e4b8250a730af066d768fba99e410e926
Kevin Hartmann <Kevin.Hartmann@oicr.on.ca>
2017-04-07 12:54:43 -0400

    Enhancement: ImportJobTest now uses an embedded MongoDb for all it's tests.
    In other words, when this test is run, it now automatically creates an embedded MongoDb instance running on an unused port.It populates the embedded database with test data (read from json files in the test_data directory), runs all the ImportJob tests, then shuts down the embedded database. This change means you no longer need to have an instance of mongodb running, with all the right collections populated with test data, just to ensure that all the tests in dcc-release pass.

--- dcc-release-job/dcc-release-job-import/src/test/java/org/icgc/dcc/release/job/imports/core/ImportJobTest.java
+++ dcc-release-job/dcc-release-job-import/src/test/java/org/icgc/dcc/release/job/imports/core/ImportJobTest.java
@@ -17,17 +17,34 @@
  */
 package org.icgc.dcc.release.job.imports.core;
 
-import lombok.val;
+import static java.lang.String.format;
 
+import java.io.File;
+
+import org.icgc.dcc.common.test.mongodb.EmbeddedMongo;
 import org.icgc.dcc.release.core.job.FileType;
 import org.icgc.dcc.release.job.imports.config.MongoProperties;
-import org.icgc.dcc.release.job.imports.core.ImportJob;
 import org.icgc.dcc.release.test.job.AbstractJobTest;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+import lombok.SneakyThrows;
+import lombok.val;
 
 public class ImportJobTest extends AbstractJobTest {
 
+  @Rule
+  public final static EmbeddedMongo embeddedMongo = new EmbeddedMongo();
+  public final static String TEST_DB = "dcc-import";
+  public final static String[] Collections = { "Project", "Gene", "GeneSet", "Drug", "Diagram" };
+  public final static String TEST_DATA_PATH = "test_data";
   /**
    * Class under test.
    */
@@ -37,7 +54,46 @@ public class ImportJobTest extends AbstractJobTest {
   @Before
   public void setUp() {
     super.setUp();
-    this.job = new ImportJob(new MongoProperties());
+
+    insertTestData(getDB());
+
+    val properties = new MongoProperties();
+    properties.setUri(getUri());
+
+    this.job = new ImportJob(properties);
+  }
+
+  private String getUri() {
+    val address = embeddedMongo.getMongo().getAddress();
+    return format("mongodb://%s:%d/%s", address.getHost(), address.getPort(), TEST_DB);
+  }
+
+  private DB getDB() {
+    return embeddedMongo.getMongo().getDB(TEST_DB);
+  }
+
+  private void insertTestData(DB db) {
+    val status = db.command("dropDatabase");
+    System.out.printf("Drop database %s returned %s\n", TEST_DB, status.toString());
+    for (val collection : Collections) {
+      System.out.printf("Adding test data for collection %s\n", collection);
+      val json = getJSON(collection);
+      populateCollection(db, collection, json);
+    }
+  }
+
+  private void populateCollection(DB db, String collection, String jsonDocument) {
+    val c = db.getCollection(collection);
+    val bson = (DBObject) JSON.parse(jsonDocument);
+
+    val status = c.insert(bson);
+    System.out.printf("Insert for collection %s returned %s\n", collection, status);
+  }
+
+  @SneakyThrows
+  private String getJSON(String collection) {
+    val f = new File(TEST_DATA_PATH, collection + ".json");
+    return Files.toString(f, Charsets.UTF_8);
   }
 
   @Test
