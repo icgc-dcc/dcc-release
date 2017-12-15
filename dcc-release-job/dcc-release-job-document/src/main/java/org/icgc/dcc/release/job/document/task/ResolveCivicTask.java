f0985cb359c90f09a65bd4fc388a6adad39f0043
Alex <lepsalex@gmail.com>
2017-12-15 11:04:54 -0500

    Civic broadcast task

---
+++ dcc-release-job/dcc-release-job-document/src/main/java/org/icgc/dcc/release/job/document/task/ResolveCivicTask.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.release.job.document.task;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Getter;
+import org.icgc.dcc.release.core.document.DocumentType;
+import org.icgc.dcc.release.core.task.TaskContext;
+import org.icgc.dcc.release.core.task.TaskType;
+import org.icgc.dcc.release.core.util.SparkWorkaroundUtils;
+
+import java.util.Map;
+
+import static org.icgc.dcc.release.core.util.Tuples.tuple;
+import static org.icgc.dcc.release.job.document.model.CollectionFieldAccessors.getClinvarVariantAnnotationId;
+
+public class ResolveClinvarTask extends AbstractDocumentTask {
+
+  @Getter
+  private Map<String, ObjectNode> annotationIdClinvar;
+
+  public ResolveClinvarTask(DocumentType type) {
+    super(type);
+  }
+
+  @Override
+  public TaskType getType() {
+    return TaskType.FILE_TYPE;
+  }
+
+  @Override
+  public void execute(TaskContext taskContext) {
+    annotationIdClinvar = readClinvar(taskContext)
+        .mapToPair(data -> tuple(getClinvarVariantAnnotationId(data), data))
+        .collectAsMap();
+
+    annotationIdClinvar = SparkWorkaroundUtils.toHashMap(annotationIdClinvar);
+  }
+
+}
